930213a05e21267d47d6cb87c965187aea2f9731
Mahule <ankit.mahule@capgemini.com>
2016-05-27 17:24:29 +0530

    design changes in all pages

--- dexmate/js/app.js
+++ dexmate/js/app.js
@@ -2,31 +2,37 @@ var app = angular.module("myapp", ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "templates/home.html",
-            controller: "homeController"
+            templateUrl: "templates/home.html"
+            , controller: "homeController"
+        })
+        .when("/catalog", {
+            templateUrl: "templates/catalog.html"
+            , controller: "catalogController"
         })
         .when("/register", {
-            templateUrl: "templates/register.html",
-            controller: "registerController"
+            templateUrl: "templates/register.html"
+            , controller: "registerController"
         })
 
         .when("/login", {
-            templateUrl: "templates/login.html",
-            controller: "loginController"
+            templateUrl: "templates/login.html"
+            , controller: "loginController"
         })
-        .otherwise({redirectTo: "templates/home.html"});
+        .otherwise({
+            redirectTo: "templates/home.html"
+        });
 
 });
 app.controller("homeController", function ($scope) {
-    $scope.load=function(){
+    $scope.load = function () {
         $('#myCarousel').carousel({
             interval: 4000
         });
 
         // handles the carousel thumbnails
-        $('[id^=carousel-selector-]').click( function(){
+        $('[id^=carousel-selector-]').click(function () {
             var id_selector = $(this).attr("id");
-            var id = id_selector.substr(id_selector.length -1);
+            var id = id_selector.substr(id_selector.length - 1);
             id = parseInt(id);
             $('#myCarousel').carousel(id);
             $('[id^=carousel-selector-]').removeClass('selected');
@@ -38,10 +44,14 @@ app.controller("homeController", function ($scope) {
             var id = $('.item.active').data('slide-number');
             id = parseInt(id);
             $('[id^=carousel-selector-]').removeClass('selected');
-            $('[id=carousel-selector-'+id+']').addClass('selected');
+            $('[id=carousel-selector-' + id + ']').addClass('selected');
         });
     };
     $scope.load();
+});
+
+app.controller("catalogController", function ($scope) {
+    $scope.message = "Hello catalog";
 });
 
 app.controller("registerController", function ($scope) {
@@ -52,29 +62,3 @@ app.controller("loginController", function ($scope) {
     $scope.message = "hello login";
 
 });
-/*
-app.controller("caraouselDemoController", function ($scope) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [{
-        "imgName": "http://cache.wallpaperdownloader.com/bing/img/WeddedRocks_20100418.jpg",
-        "caption": "first"
-        },
-        {
-            "imgName": "http://cache.wallpaperdownloader.com/bing/img/WeddedRocks_20100418.jpg",
-            "caption": "second"
-        },
-        {
-            "imgName": "http://cache.wallpaperdownloader.com/bing/img/WeddedRocks_20100418.jpg",
-            "caption": "third"
-        },
-        {
-            "imgName": "http://cache.wallpaperdownloader.com/bing/img/WeddedRocks_20100418.jpg",
-            "caption": "four"
-        },
-        {
-            "imgName": "http://cache.wallpaperdownloader.com/bing/img/WeddedRocks_20100418.jpg",
-            "caption": "five"
-        }
-    ];
-});
-*/
